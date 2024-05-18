00d0eacbcce83aa358c97f874cc6b81664c43019
Ratatog <156364201+Ratatog@users.noreply.github.com>
2024-05-18 17:21:45 +0500

    hw.py

--- Hw19/hw.py
+++ Hw19/hw.py
@@ -8,3 +8,21 @@ for word in texts:
     res = re.search(answer, word.lower())
     if res:
         print(word)
+
+# 2. Соответсвие всем url-адресам.
+answer = r'https?:/(?:/[^/]+)+/?'
+text = 'https://ya.ru/search/'
+
+print( re.findall(answer, text) )
+
+# 3. Хотя бы одно слово с заглавной буквы
+answer = r'\s[А-ЯA-Z]'
+text = 'один Два три четыре Пять'
+
+print( re.search(answer, text) )
+
+# 4. Строки с повторяющейся буквой
+answer = r'(\S)\1'
+text = 'Поезда Светофоры Картонные'
+
+print( re.search(answer, text) )
